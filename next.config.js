0aabb4d1f6afe531b0a093f870bbc05055c3c70a
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-07 04:18:36 +0900

    revert: modernモードでのビルドを一時取り止める
    Refs: 53fcd27

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    modern: true
-  },
   webpack(config, { defaultLoaders }) {
     config.module.rules.push({
       test: /\.(?:jpe?g|webp)$/,
