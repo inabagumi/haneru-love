78f22abae6f057af4313017ef3dc9ab0dabdd29a
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-11-07 12:20:27 +0900

    feat(hero): disable three (#329)

--- next.config.js
+++ next.config.js
@@ -1,36 +1,8 @@
-const nextTranspileModules = require('next-transpile-modules')
-
-const withTM = nextTranspileModules(['postprocessing', 'three'], {
-  unstable_webpack5: true
-})
-
-/**
- * @typedef {Object} DefaultLoaders
- * @property {import('webpack').RuleSetRule} babel
- */
-
-/**
- * @typedef {Object} NextWebpackOptions
- * @property {string} buildId
- * @property {boolean} dev
- * @property {boolean} isServer
- * @property {DefaultLoaders} defaultLoaders
- */
-
-/**
- * @type {import('next').NextConfig}
- * @see https://nextjs.org/docs/api-reference/next.config.js/introduction
- */
 const nextConfig = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja']
   },
-  /**
-   * @param {import('webpack').Configuration} config
-   * @param {NextWebpackOptions} options
-   * @see https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-   */
   webpack(config, { defaultLoaders }) {
     config.module.rules.push({
       test: /\.(?:jpe?g|png|webp)$/,
@@ -52,4 +24,4 @@ const nextConfig = {
   }
 }
 
-module.exports = withTM(nextConfig)
+module.exports = nextConfig
