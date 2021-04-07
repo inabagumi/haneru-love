188307defdca7af8b786157444babd920ab23299
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-04-07 17:27:31 +0900

    fix(icon-button): use ForwardRefComponent

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,24 +1,27 @@
-import { forwardRefWithAs } from '@reach/utils'
+import type { ForwardRefComponent } from '@reach/utils'
 import clsx from 'clsx'
+import { forwardRef } from 'react'
 import styles from '../styles/components/icon-button.module.css'
 
 type Props = {
   disabled?: boolean
 }
 
-const IconButton = forwardRefWithAs<Props, 'button'>(function IconButton(
-  { as: Comp = 'button', children, className, ...props },
-  forwardedRef
-) {
-  return (
-    <Comp
-      className={clsx(styles.iconButton, className)}
-      ref={forwardedRef}
-      {...props}
-    >
-      {children}
-    </Comp>
-  )
-})
+const IconButton: ForwardRefComponent<'button', Props> = forwardRef(
+  function IconButton(
+    { as: Comp = 'button', children, className, ...props },
+    forwardedRef
+  ) {
+    return (
+      <Comp
+        className={clsx(styles.iconButton, className)}
+        ref={forwardedRef}
+        {...props}
+      >
+        {children}
+      </Comp>
+    )
+  }
+)
 
 export default IconButton
