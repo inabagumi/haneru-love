c241eac6c5802d5ad3ca786d398ac2499b6dca7b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-08-08 23:13:29 +0900

    fix(icon-button): fix type

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -7,21 +7,19 @@ type Props = {
   disabled?: boolean
 }
 
-const IconButton: ForwardRefComponent<'button', Props> = forwardRef(
-  function IconButton(
-    { as: Comp = 'button', children, className, ...props },
-    forwardedRef
-  ) {
-    return (
-      <Comp
-        className={clsx(styles.iconButton, className)}
-        ref={forwardedRef}
-        {...props}
-      >
-        {children}
-      </Comp>
-    )
-  }
-)
+const IconButton = forwardRef(function IconButton(
+  { as: Comp = 'button', children, className, ...props },
+  forwardedRef
+) {
+  return (
+    <Comp
+      className={clsx(styles.iconButton, className)}
+      ref={forwardedRef}
+      {...props}
+    >
+      {children}
+    </Comp>
+  )
+}) as ForwardRefComponent<'button', Props>
 
 export default IconButton
